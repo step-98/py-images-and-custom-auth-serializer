39b53620f93c55e15d8175d51e5ce2a87bf64c84
Vitaliy Stepanyuk <vist98@gmail.com>
2026-06-23 19:59:25 +0300

    Fixed solution

--- cinema/models.py
+++ cinema/models.py
@@ -42,8 +42,8 @@ class Actor(models.Model):
 def movie_image_path(instance: "Movie", filename: str):
     _, ext = os.path.splitext(filename)
     return os.path.join(
-        "upload/images/",
-        f"{slugify(instance.title)}-{uuid}{ext}"
+        "uploads/images/",
+        f"{slugify(instance.title)}-{uuid.uuid4()}{ext}"
     )
 
 
@@ -53,7 +53,7 @@ class Movie(models.Model):
     duration = models.IntegerField()
     genres = models.ManyToManyField(Genre)
     actors = models.ManyToManyField(Actor)
-    image = models.ImageField(null=True, upload_to="movie_image_path")
+    image = models.ImageField(null=True, upload_to=movie_image_path)
 
     class Meta:
         ordering = ["title"]
